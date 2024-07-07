04c42a14df055ec8f72c3041c0bf3f255782ba8d
farrizjhdy <ryyzzz7@gmail.com>
2024-07-07 10:12:42 +0700

    update dark mode

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function() {
     const header = document.querySelector('header')
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if(window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -19,3 +24,33 @@ hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active')
     navMenu.classList.toggle('hidden');
 })
+
+
+// Klik Diluar hamburger
+window.addEventListener('click', function (e) {
+    if(e.target != hamburger && e.target!= navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+// Dark Mode toggle
+const darktoggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darktoggle.addEventListener('click', function( ) {
+    if (darktoggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+// Pindahan Toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darktoggle.checked = true;
+  } else {
+    darktoggle.checked = false;
+  }
